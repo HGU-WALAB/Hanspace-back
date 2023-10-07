737c8ed3c5d088cf2b8b340815cc3614639fc49c
healim01 <22000770@handong.edu>
2023-10-07 23:49:05 +0900

    feat: cors error fix

--- src/main/java/com/example/hanspaceback/controller/SpaceTimeExtraController.java
+++ src/main/java/com/example/hanspaceback/controller/SpaceTimeExtraController.java
@@ -11,6 +11,7 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
+@CrossOrigin("*")
 public class SpaceTimeExtraController {
     private final SpaceTimeExtraService spaceTimeExtraService;
     @PostMapping("/spaceTimeExtra")
