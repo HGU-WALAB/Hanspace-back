5a9c14ce9bb7101b855e22fed380ff5b5c1841c0
uojin29 <uojin29@gmail.com>
2023-10-10 16:47:44 +0900

    feat : create ReserveFindByDeptId, ReserveFindBySpaceID

--- src/main/java/com/example/hanspaceback/repository/ReserveRepository.java
+++ src/main/java/com/example/hanspaceback/repository/ReserveRepository.java
@@ -1,6 +1,7 @@
 package com.example.hanspaceback.repository;
 
 import com.example.hanspaceback.domain.Reserve;
+import com.example.hanspaceback.domain.Space;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -11,4 +12,6 @@ import java.util.List;
 public interface ReserveRepository extends JpaRepository<Reserve, Long> {
     @Query("select r from Reserve r left join fetch r.member m left join fetch r.space c left join fetch r.regularReserve lr")
     List<Reserve> findReserveFetchJoin();
+    List<Reserve> findBySpace_SpaceId(Long spaceId);
+    List<Reserve> findBySpace_Department_deptId(Long dept_id);
 }
--- src/main/java/com/example/hanspaceback/service/ReserveService.java
+++ src/main/java/com/example/hanspaceback/service/ReserveService.java
@@ -38,52 +38,18 @@ public class ReserveService {
                 .build();
         reserveRepository.save(reserve);
     }
-    public void createRegular(ReserveRequest request, int i){
-        Space space = spaceRepository.findById(request.getSpaceId()).orElseThrow();
-        Member member = memberRepository.findById(request.getMemberId()).orElseThrow();
-        Reserve reserve = Reserve.builder()
-                .reserveDate(request.getReserveDate()[i])
-                .startTime(request.getStartTime())
-                .endTime(request.getEndTime())
-                .headCount(request.getHeadCount())
-                .groupName(request.getGroupName())
-                .purpose(request.getPurpose())
-                .phoneNumber(request.getPhoneNumber())
-                .approve(request.getApprove())
-                .extraInfoAns(request.getExtraInfoAns())
-                .member(member)
-                .space(space)
-                .build();
-        reserveRepository.save(reserve);
-    }
 
     public List<Reserve> findReserveFetchJoin(){
         return reserveRepository.findReserveFetchJoin();
     }
-//    public List<ReserveResponse> findReserveFetchJoin(){
-//        List<Reserve> reserves = new ArrayList<>();
-//        List<ReserveResponse> responses = new ArrayList<>();
-//
-//        for(Reserve reserve : reserves){
-//            ReserveResponse response = new ReserveResponse();
-//            response.setReserveId(reserve.getId());
-//            response.setSpaceId(reserve.getSpace().getSpaceId());
-//            response.setMemberId(reserve.getMember().getMemberId());
-//            response.setLongReserveId(reserve.getRegularReserve().getId());
-//            response.setReserveDate(reserve.getReserveDate());
-//            response.setStartTime(reserve.getStartTime());
-//            response.setEndTime(reserve.getEndTime());
-//            response.setHeadCount(reserve.getHeadCount());
-//            response.setGroupName(reserve.getGroupName());
-//            response.setPurpose(reserve.getPurpose());
-//            response.setPhoneNumber(reserve.getPhoneNumber());
-//            response.setApprove(reserve.getApprove());
-//            response.setExtraInfoAns(reserve.getExtraInfoAns());
-//
-//            responses.add(response);
-//        }
-//        return responses;
-//    }
+    public List<Reserve> findBySpaceId(Long spaceId){
+        List<Reserve> reserves = reserveRepository.findBySpace_SpaceId(spaceId);
+        return reserves;
+    }
+    public List<Reserve> findByDeptId(Long deptId){
+        List<Reserve> reserves = reserveRepository.findBySpace_Department_deptId(deptId);
+        return reserves;
+    }
 
     public Reserve update(Long id, ReserveRequest request){
         Reserve reserve = reserveRepository.findById(id).orElseThrow();
