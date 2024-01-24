46f8c626bf173c96fc18600631d3634e2bc76bf6
uojin29 <uojin29@gmail.com>
2024-01-25 00:33:27 +0900

    #69/refactor: data.sql 수정 (member table의 새로운 필드 값 넣음)

--- src/main/java/com/example/hanspaceback/domain/Member.java
+++ src/main/java/com/example/hanspaceback/domain/Member.java
@@ -27,6 +27,7 @@ public class Member extends BaseEntity{
     @Column(unique = true)
     private String email;
     private HanRole hanRole;
+
     @Column(unique = true)
     private String sId;
     private String password;
